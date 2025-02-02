e6b0556d36599eeb58a2fb80902451edb5dab439
Ruben Coppens <ruben.coppens@outlook.com>
2025-02-02 20:53:14 +0100

    Timer made

--- Code/LCD.py
+++ Code/LCD.py
@@ -2,18 +2,30 @@
 
 import LCD1602
 import time
+from datetime import datetime
 
 def setup():
 	LCD1602.init(0x27, 1)	# init(slave address, background light)
-	LCD1602.write(0, 0, 'MILAWI ILY <3')
-	LCD1602.write(1, 1, 'X HOMBRE')
 	time.sleep(2)
 
 def destroy():
 	LCD1602.clear()
 
+def update_time():
+	current_timestamp = datetime.now()
+	date_cur = current_timestamp.strftime('%Y-%m-%d')
+	time_cur = current_timestamp.strftime('%H:%M:%S')
+	LCD1602.write(0, 0, date_cur)
+	LCD1602.write(1, 1, time_cur)
+
+
+
+
 if __name__ == "__main__":
 	try:
 		setup()
+		while True:
+			update_time()
+			time.sleep(0.1)
 	except KeyboardInterrupt:
 		destroy()
